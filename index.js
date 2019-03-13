28763b031096929be196c6595521c763d11298ac
Zach Schwartz <zischwartz@gmail.com>
2019-03-13 12:52:32 -0400

    got going

--- index.js
+++ index.js
@@ -8,12 +8,18 @@ const index_template = fs.readFileSync(
   path.resolve(__dirname, "src/index.html"),
   "utf8"
 );
+// const page_template = fs.readFileSync(
+//   path.resolve(__dirname, "src/page.html"),
+//   "utf8"
+// );
 
 // let items_sheet_url =
 //   "https://sheets.googleapis.com/v4/spreadsheets/SPREADSHEET_ID/WORKSHEET_TAB_NAME?key=YOUR_API_KEY";
 
 let items_sheet_url =
   "https://sheets.googleapis.com/v4/spreadsheets/12V1CZtjXPIio2Ar3-5Vd0DpGnjxYe2jL1jHgc1Zpk_o/values/items?key=AIzaSyAqttGhmNJpCrkhJ3Qnj9KHFmTeL8KXjuI";
+
+let content_sheet_url = `https://sheets.googleapis.com/v4/spreadsheets/12V1CZtjXPIio2Ar3-5Vd0DpGnjxYe2jL1jHgc1Zpk_o/values/content?key=AIzaSyAqttGhmNJpCrkhJ3Qnj9KHFmTeL8KXjuI`;
 
 request(items_sheet_url, function(error, response, body) {
   // console.log("error:", error); // Print the error if one occurred
@@ -34,6 +40,8 @@ function create_item_objects_from_raw(values) {
       item[headings[i]] = v;
     }
     item["slug"] = slugify(item.title);
+    // stick the original row back in
+    item["row"] = row;
     result.push(item);
     // console.log(row);
   }
@@ -46,7 +54,26 @@ function create_site(items) {
   let index_items_content = create_index_items_content(items);
   fs.writeFileSync(
     `dist/index.html`,
-    render(index_template, { index_items_content, title })
+    render(index_template, { content: index_items_content, title })
+  );
+
+  items.forEach(item => create_item_page(item));
+}
+
+function create_item_page(item) {
+  function make_img(p) {
+    // this only works for offline xxx
+    return `<img src="../../images/${p}"/>`;
+  }
+
+  let title = `${item.title}`;
+  let image_paths = item.row.slice(4);
+  let content = `<div>${image_paths.map(make_img)}</div>`;
+  let path = `dist/${item.slug}`;
+  fs.mkdirSync(path);
+  fs.writeFileSync(
+    `${path}/index.html`,
+    render(index_template, { content, title })
   );
 }
 
@@ -59,10 +86,12 @@ function create_index_items_content(items) {
     // for local testing
     let src =
       process.env.NODE_ENV != "production"
-        ? `../images/${piece.cover_image}`
-        : `images/${piece.cover_image}`;
+        ? `../images/${piece.cover_image}` // it's up one when just loadin git in dev, because it's in dist
+        : `images/${piece.cover_image}`; // in prod, images will be at the top level of dist
     // for prod, we'll need a thing to copy all the images over to dist
-    result += `<a href="${piece.slug}/"><img src="${src}"></a>`;
+    result += `<div class="thumb"><a href="${
+      piece.slug
+    }/"><img src="${src}"></a></div>`;
   }
   return result;
 }
